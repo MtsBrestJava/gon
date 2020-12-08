fdb8b29dbaf8183e19ebdeddf69d4c5c8d53aa01
vik22tor <75635868+vik22tor@users.noreply.github.com>
2020-12-08 22:11:56 +0300

    Update Pokupki.java

--- src/by/mts/brest/gondyuk/Pokupki.java
+++ src/by/mts/brest/gondyuk/Pokupki.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class Pokupki {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Укажите категорию продукта продукта, поставив число (1 - жидкость, 2 - сыпучий, 3 - штучный)");
-            byte num;
-            byte start;
-            try {
-                 num = in.nextByte();
-            }
-            catch (Exception e) {
-                //e.printStackTrace();
-                num = 0;
-                System.out.println("Введите число > 0");
-            }
-        if num = 1;
-            ;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Укажите категорию продукта продукта, поставив число (1 - жидкость, 2 - сыпучий, 3 - штучный): ");
+        //byte start;
+        int num;
+        int x;
+        try {
+            num = s.nextInt();
+        } //if (num > 0);
+        catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Перезапустите программу и введите целое число > 0");
+        }
+        //in.nextLine();
+        //if num = 1;
     }
-
 }
