c3dacff0bdde8e96e2fa314b1ed4a2461935d28a
vik22tor <75635868+vik22tor@users.noreply.github.com>
2020-12-12 00:15:23 +0300

    Create Pokupki.class

--- src/by/mts/brest/gondyuk/Pokupki.java
+++ src/by/mts/brest/gondyuk/Pokupki.java
@@ -4,19 +4,29 @@ import java.util.Scanner;
 
 public class Pokupki {
     public static void main(String[] args) {
+        Product product1 = new Product();
         Scanner s = new Scanner(System.in);
         System.out.println("Укажите категорию продукта продукта, поставив число (1 - жидкость, 2 - сыпучий, 3 - штучный): ");
-        //byte start;
         int num;
-        int x;
         try {
             num = s.nextInt();
-        } //if (num > 0);
+            product1.id = num;
+        }
         catch (Exception e) {
             //e.printStackTrace();
             System.out.println("Перезапустите программу и введите целое число > 0");
         }
-        //in.nextLine();
-        //if num = 1;
+    }
+
+    static class Product{
+        private int id;
+
+        public void setId(){
+            this.id = id;
+        }
+        public int getId(){
+            return id;
+        }
     }
 }
+
